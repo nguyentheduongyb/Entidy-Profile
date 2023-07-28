9b660ce19ef4e996e1b1ffcf0de6c14e3c199a9b
nguyentheduongyb <nguyentheduongyb@gmail.com>
2023-07-28 12:46:15 +0700

    remove header

--- layout/DefaultLayout.tsx
+++ layout/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import Footer from "~/components/common/Footer";
 const DefaultLayout = ({ children }: PropsWithChildren) => {
         return (
                 <>
-                        <Header />
+                        {/* <Header /> */}
                         <main className="w-full min-h-screen">
                                 {children}
                         </main>
