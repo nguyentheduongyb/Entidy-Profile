35e650449750f2a1b7e19e9aed93de21c60ec6c9
nguyentheduongyb <nguyentheduongyb@gmail.com>
2024-05-29 18:27:43 +0700

    shop

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -36,7 +36,20 @@ const Shop = () => {
                                                 <br />
                                                 <span>Địa chỉ: 176 Điện Biên, Thành Phố Yên Bái, Tỉnh Yên Bái.</span>
                                         </p>
-
+                                        <div className="mt-4 flex justify-center space-x-3 lg:mt-6">
+                                                <a
+                                                        href="https://www.facebook.com/profile.php?id=100021603316490"
+                                                        className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                                                >
+                                                        Open Facebook
+                                                </a>
+                                                <a
+                                                        href="https://m.me/100021603316490"
+                                                        className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                                                >
+                                                        Message
+                                                </a>
+                                        </div>
                                 </div>
                         </div>
                 </div >
