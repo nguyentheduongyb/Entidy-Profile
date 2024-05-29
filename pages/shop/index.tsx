f9b4a69b7049375c1ef8cd373802220ec40888a8
nguyentheduongyb <nguyentheduongyb@gmail.com>
2024-05-29 22:44:58 +0700

    shop

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -43,13 +43,13 @@ const Shop = () => {
                                                         href="https://www.facebook.com/profile.php?id=100021603316490"
                                                         className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
                                                 >
-                                                        Open Facebook
+                                                        Xem tất cả sản phẩm đang bán
                                                 </a>
                                                 <a
                                                         href="https://m.me/100021603316490"
                                                         className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                                                 >
-                                                        Message
+                                                        Nhắn tin
                                                 </a>
                                         </div>
                                 </div>
